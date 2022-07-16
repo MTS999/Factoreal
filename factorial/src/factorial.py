7f66db0f3bc3c4728a6067be1e8225033f0a0370
MTS999 <mtsprogrammer999@gmail.com>
2022-07-16 15:45:13 +0500

    refector

--- factorial/src/factorial.py
+++ factorial/src/factorial.py
@@ -4,12 +4,12 @@ def factorial(n):
          print("Sorry, factorial does not exist for negative nbers")
     elif n == 0:
          print("The factorial of 0 is 1")
+    else: 
     
-    for i in range(1,n + 1):
-         a = a*i
+         for i in range(1,n + 1):
+             a = a*i
         
         
-    print(a)
+         print(a)
     
  
-    
