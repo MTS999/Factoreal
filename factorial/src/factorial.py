50ca8508c26ca3c16486ae5835d9a1882948d324
MTS999 <mtsprogrammer999@gmail.com>
2022-07-17 10:29:18 +0500

    add return value

--- factorial/src/factorial.py
+++ factorial/src/factorial.py
@@ -1,15 +1,11 @@
 def factorial(n):
-    a=1
-    if n < 0:
-         print("Sorry, factorial does not exist for negative nbers")
-    elif n == 0:
-         print("The factorial of 0 is 1")
-    else: 
-    
-         for i in range(1,n + 1):
-             a = a*i
+     a=1
+     if n == 0:
+         return 1
+     for i in range(1,n + 1):
+          a = a*i
         
         
-         print(a)
+     return a
     
  
